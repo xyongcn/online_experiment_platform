64ea3539eb43c53bd0e7436801df2c0d3f8e6a68
rainymoon911 <zhangrain911@163.com>
2015-09-30 20:15:59 +0800

    change default port

--- XBlock/xblock-codebrowser/codebrowser/config.py
+++ XBlock/xblock-codebrowser/codebrowser/config.py
@@ -5,7 +5,7 @@ class Config:
         {
             # GitLab's host and port, make sure open-edx can access
             "HOST" : "192.168.1.62",
-            "PORT" : 80
+            "PORT" : 22
         }
     }
 
