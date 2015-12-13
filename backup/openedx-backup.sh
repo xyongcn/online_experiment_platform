d5f7ec6d820857a4ccf5275bdc3306be324252b6
rainymoon911 <zhangrain911@163.com>
2015-12-13 15:51:01 +0800

    Update openedx-backup.sh

--- backup/openedx-backup.sh
+++ backup/openedx-backup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 DATE=$(date +%Y%m%d)
 file_name="/mnt/date_backup/openedx/mongo_dump-"-$DATE
+mongo_user="secret"
 mongo_pwd="secret"
-mongodump -u edxapp -p $mongo_pwd -o $file_name
+mongodump -u mongo_user -p $mongo_pwd -o $file_name
 mv /mnt/data_backup/*.ldif /mnt/data_backup/ldap/
