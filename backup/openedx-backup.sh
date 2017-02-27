317d50a5aa2acb4c51e8954b91fd73f801f08037
rainymoon911 <zhangrain911@163.com>
2017-02-27 14:59:14 +0800

    add local backup

--- backup/openedx-backup.sh
+++ backup/openedx-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 DATE=$(date +%Y%m%d)
-file_name="/mnt/date_backup/openedx/mongo_dump-"-$DATE
+file_name="/root/tmp_mongo_backup/mongo_dump-"$DATE
 mongo_user="secret"
 mongo_pwd="secret"
-mongodump -u mongo_user -p $mongo_pwd -o $file_name
+mongodump -u $mongo_user -p $mongo_pwd -o $file_name
+mv $file_name /mnt/data_backup/openedx/
 mv /mnt/data_backup/*.ldif /mnt/data_backup/ldap/
